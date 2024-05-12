fd8e1c89822b3180db4a9ae067857247c5277b04
yuta-takeda <ytakeda@jmdc.co.jp>
2024-05-12 10:48:35 +0900

    ui container

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import lightModeIcon from "./assets/icons/light-mode.svg";
+import emptyState from "./assets/emptystate.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <div className="title">TODO</div>
+        <div className="toggle">
+          <img src={lightModeIcon} alt={"モード切替"} />
+        </div>
+      </div>
+      <div className="background">
+        <div className="addTodo">
+          <input
+            type="text"
+            className="input"
+            placeholder="Create a new todo..."
+          />
+          <button type="button" className="button">
+            Add todo
+          </button>
+        </div>
+        <div className="todoList">
+          <img src={emptyState} className="emptyState" alt="no todo" />
+          <p>You're todo's are empty</p>
+          <small>Please add first one</small>
+        </div>
+      </div>
     </div>
   );
 }
